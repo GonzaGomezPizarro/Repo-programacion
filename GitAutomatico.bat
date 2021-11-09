6e533146caa6013ab384e1ab93e84561d2b99bc6
GonzaGomezPizarro <11912988@ucc.edu.ar>
2021-11-09 17:26:56 -0300

    Repositorio atualizado

--- GitAutomatico.bat
+++ GitAutomatico.bat
@@ -1,16 +1,41 @@
 @echo off
 title GIT AUTOMATICO (add, commit, pull, push)...
 echo.
-echo  HOLA, ejecutando comandos... 
-echo.
+echo ------------------------------------
+echo -   HOLA, ejecutando comandos...   -
+echo ------------------------------------
 
+echo.
+echo -------------------
+echo -    GIT ADD .    -
+echo -------------------
 git add .
+
+echo.
+echo -------------------
+echo -  GIT COMMIT -M  - 
+echo -------------------
+echo.
 git commit -m "Repositorio atualizado"
+
+echo.
+echo --------------------
+echo -     GIT PULL     -
+echo --------------------
+echo.
 git pull
+
+echo.
+echo --------------------
+echo -     GIT PUSH     -
+echo --------------------
+echo.
 git push
 
 echo.
-echo Proceso finalizado... revise errores
+echo ----------------------------------------
+echo - Proceso finalizado... revise errores -
+echo ----------------------------------------
 echo.
 echo  Presione una tecla para continuar...
 pause>nul
