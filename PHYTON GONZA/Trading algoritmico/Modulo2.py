8ab82ef3106f9dabb9f3d1284fcf05cb97167178
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-01-19 19:45:00 -0300

    Repositorio atualizado

--- PHYTON GONZA/Trading algoritmico/Modulo2.py
+++ PHYTON GONZA/Trading algoritmico/Modulo2.py
@@ -1,5 +1,9 @@
 # clase 1;
+import string
 from turtle import color
+from xml.etree.ElementTree import tostring
+
+from numpy import size
 import funciones
 import matplotlib.pyplot as plt
 import yfinance as yf
@@ -9,10 +13,13 @@ plt.style.use('dark_background')
 data = funciones.BajarDatos('GE', '2018-01-01') 
 mediamovil200 = data['Close'].rolling(window=200).mean()
 mediamovil50 = data['Close'].rolling(window=50).mean()
+mediamovilexponecial200 = data['Close'].ewm(span=200).mean()    
 upper_rollingerBand = mediamovil200 + (3 * data['Close'].rolling(window=200).std())
 lower_rollingerBand = mediamovil200 - (3 * data['Close'].rolling(window=200).std())
 
-data.Close.plot(label='GGAL')
+data.ticker = data.ticker.str.replace('.', '-')
+
+data.Close.plot(label='GGAL' )
 print("-> Precios galicia: \n", data)
 
 plt.plot(mediamovil200, label='Media 200', color='red')
@@ -20,6 +27,5 @@ plt.plot(mediamovil200, label='Media 200', color='red')
 plt.plot(upper_rollingerBand, label='Upper Bollinger Band', color='blue')
 plt.plot(lower_rollingerBand, label='Lower Bollinger Band', color='blue')
 plt.show()
-
 
 
