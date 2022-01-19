47571be2d2d7c15eac53819969cd18b48f105176
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-01-19 11:33:17 -0300

    Repositorio atualizado

---
+++ Git status.bat
@@ -0,0 +1,17 @@
+@echo off
+title FIT STATUS
+echo.
+echo ------------------------------------
+echo -   HOLA, ejecutando comando...   -
+echo ------------------------------------
+
+
+echo.
+echo ----------------------
+echo -     git status     -
+echo ----------------------
+echo.
+git status
+
+echo  Presione una tecla para finalizar...
+pause>nul
