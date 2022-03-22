df5c4ffa18d929fa8c3cda7270c1f1415d9178cc
GonzaGomezPizarro <11912988@ucc.edu.ar>
2022-03-22 17:06:44 -0300

    Repositorio atualizado

--- ArquiDeSoftware/main.go
+++ ArquiDeSoftware/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func main () {
-	fmt.Println("Hello World")
+func division(a, b float32) (float32, error) {
+	if b == 0 {
+		errorr := errors.New("No se puede dividir por cero")
+		return 999999, errorr
+	}
+	var res float32 = a / b
+	return res, nil
+}
+
+func div(a, b int) (int, int) {
+	return a / b, a % b
+}
+
+func main() {
+
+	var dividendo, divisor float32 = 10, 3
+
+	div, err := division(dividendo, divisor)
+
+	if err != nil {
+		fmt.Println("Error: ", err, "  |  Division: ", div)
+	} else {
+		fmt.Println("Resultado: ", div, "  |  Error: ", err)
+	}
+
 }
