9f23d43355d7cfcd805ffa10fed3c957ca22e653
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-10-03 11:47:14 -0300

    :

--- PrimerParcialAS2/TOPIC QUEUES/send.py
+++ PrimerParcialAS2/TOPIC QUEUES/send.py
@@ -4,7 +4,7 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='topicss', exchange_type='topic',) 
+channel.exchange_declare(exchange='topicss', exchange_type='topic') 
 
 clave = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
 mensaje = ' '.join(sys.argv[2:]) or 'Hello World!'
